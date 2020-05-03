e4d6ea8d45c5a29896a8fe9d2c47de7b6e1ffafa
Ava Smith <jsmit208@uic.edu>
2020-05-03 18:53:44 +0000

    Added cdn caching

--- sw.js
+++ sw.js
@@ -13,10 +13,26 @@ self.addEventListener("install", installEvent => {
   )
 })
 
+/*
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request)
     })
   )
-})
+}) */
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(staticDangersOfBiking).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
